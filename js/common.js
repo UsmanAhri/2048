a485be4a4c03efb92b0c33c9fbbe872f1fcde970
UsmanAhri <47859269+UsmanAhri@users.noreply.github.com>
2019-09-09 16:56:54 +0300

    added blocks with numbers, centered the numbers inside the block, added a different background to blocks with different numbers, added the function of creating new blocks

--- js/common.js
+++ js/common.js
@@ -1,24 +1,24 @@
-let selectedBlockSize = Number(document.querySelector('.selected-size').value);
+let selectedBlockAmount = Number(document.querySelector('.selected-size').value);
 
 const c = document.querySelector('.container');
-const indexs = document.querySelectorAll('.index');
+const indexItems = document.querySelectorAll('.index');
 let cur = -1;
-indexs.forEach((index, i) => {
+indexItems.forEach((index, i) => {
     index.addEventListener('click', () => {
-        for (let j = 0; j < indexs.length; j++) {
-            indexs[j].classList.remove('selected-size');
+        for (let j = 0; j < indexItems.length; j++) {
+            indexItems[j].classList.remove('selected-size');
         }
-        indexs[i].classList.add('selected-size');
-        selectedBlockSize = Number(document.querySelector('.selected-size').value);
+        indexItems[i].classList.add('selected-size');
+        selectedBlockAmount = Number(document.querySelector('.selected-size').value);
 
-        block.coordinates = [
+        staticBlock.coordinates = [
             {
-                x: canv.borderSise,
-                y: canv.borderSise
+                x: canv.borderSize,
+                y: canv.borderSize
             }
         ];
-        block.blockSizeCalculation();
-        block.staticBlocksCoordinatesRecord();
+        staticBlock.blockSizeCalculation();
+        staticBlock.staticBlocksCoordinatesRecord();
 
         c.className = 'container';
         void c.offsetWidth; // Reflow
@@ -32,30 +32,55 @@ indexs.forEach((index, i) => {
 });
 //-----------------------------------------//
 
-function Canvas() {
-    this.obj = document.getElementById('canvas');
-    this.width = 500;
-    this.height = 500;
-    this.borderSise = 5;
-}
-
-let canv = new Canvas();
+let canv = {
+    obj: document.getElementById('canvas'),
+    width: 500,
+    height: 500,
+    borderSize: 5,
+};
 
 const ctx = canvas.getContext("2d");
 
-canvas.width = canv.width;
-canvas.height = canv.height;
+canv.obj.width = canv.width;
+canv.obj.height = canv.height;
 
 function Block() {
-    this.size = Math.round((canv.width - canv.borderSise * (selectedBlockSize + 1)) / selectedBlockSize);
-    this.coordinates = [
+
+}
+
+Block.prototype = {
+    size: Math.round((canv.width - canv.borderSize * (selectedBlockAmount + 1)) / selectedBlockAmount),
+    coordinates: [
         {
-            x: canv.borderSise,
-            y: canv.borderSise
+            x: canv.borderSize,
+            y: canv.borderSize
         }
-    ];
-    this.blockSizeCalculation = function() {
-        switch (selectedBlockSize) {
+    ],
+    centeringNumbers:[],
+    numbers: ["2048"],
+    numberSize: "50px",
+    numberBlocksCoordinates: [
+        {
+            x: canv.borderSize,
+            y: canv.borderSize
+        }
+    ],
+    numberBlocksBackgroundColors: {
+        2: "#eee4da",
+        4: "#ece0c7",
+        8: "#f2b179",
+        16: "#fa9360",
+        32: "#fd775f",
+        64: "#f85b40",
+        128: "#edcf72",
+        256: "#edcc5f",
+        512: "#ecc657",
+        1024: "#eec53d",
+        2048: "#eec22e",
+        larger: "#3d3a33"
+    },
+    blockSizeCalculation: function () {
+        switch (selectedBlockAmount) {
             case 3:
                 this.size = 160;
                 break;
@@ -78,18 +103,17 @@ function Block() {
                 this.size = 160;
         }
         return this.size;
-    };
-    this.staticBlocksCoordinatesRecord = function() {
-        let blockX, blockY = canv.borderSise;
+    },
+    staticBlocksCoordinatesRecord: function () {
+        let blockX, blockY = canv.borderSize;
 
-        for (let i = 0; i < selectedBlockSize; i++) {
-            blockX = canv.borderSise;
-            for (let j = 0; j < selectedBlockSize; j++) {
+        for (let i = 0; i < selectedBlockAmount; i++) {
+            blockX = canv.borderSize;
+            for (let j = 0; j < selectedBlockAmount; j++) {
                 if (j === 0) {
-                    blockX = canv.borderSise;
-                }
-                else {
-                    blockX = this.coordinates[j].x + this.size + canv.borderSise;
+                    blockX = canv.borderSize;
+                } else {
+                    blockX = this.coordinates[j].x + this.size + canv.borderSize;
                 }
 
                 this.coordinates.push({
@@ -97,28 +121,117 @@ function Block() {
                     y: blockY
                 });
             }
-            blockY = this.coordinates[this.coordinates.length - 1].y + this.size + canv.borderSise;
+            blockY = this.coordinates[this.coordinates.length - 1].y + this.size + canv.borderSize;
         }
         this.coordinates.shift();
+    },
+    centeringNumberCoordinatesRecord: function() {
+        let centerX, centerY;
+
+        for (let i = 0; i < this.numbers.length; i++) {
+            centerX = this.numberBlocksCoordinates[i].x + Math.round(this.size / 2);
+            centerY = this.numberBlocksCoordinates[i].y + Math.round(this.size / 2);
+             this.centeringNumbers.push({
+                 x: centerX,
+                 y: centerY
+             })
+        }
+    },
+    allNumberCoordinatesRecord: function() {
+        let centerX, centerY;
+
+        for (let i = 0; i < this.numbers.length; i++) {
+            centerX = this.numberBlocksCoordinates[i].x + Math.round(this.size / 2);
+            centerY = this.numberBlocksCoordinates[i].y + Math.round(this.size / 2);
+             this.centeringNumbers.push({
+                 x: centerX,
+                 y: centerY
+             })
+        }
+    },
+    createBlock: function () {
+        console.log(this);
+        if (this.numbers.length < this.coordinates.length) {
+            this.numbers.push("2");
+            this.numberBlocksCoordinates.push(
+                this.coordinates[Math.round(Math.random() * (this.coordinates.length - 1))]
+            )
+        }
     }
+};
+//Not sure if constructor needs to be restored
+Object.defineProperty(Block.prototype, "constructor", {
+    enumerable: false,
+    value: Block
+});
+
+let staticBlock = new Block();
+
+staticBlock.staticBlocksCoordinatesRecord();
+
+let blocksWithNumbers = new Block();
+
+blocksWithNumbers.centeringNumberCoordinatesRecord();
+blocksWithNumbers.createBlock();
+
+function fas() {
+    blocksWithNumbers.createBlock();
 }
 
-let block = new Block();
+document.addEventListener('keydown', fas);
 
 
 
 
-block.staticBlocksCoordinatesRecord();
+function canvasRadius(x, y, w, h, rad) {
+    let r = x + w,
+        b = y + h;
+
+    ctx.beginPath();
+    ctx.moveTo(x + rad, y);
+    ctx.lineTo(r - (rad), y);
+    ctx.quadraticCurveTo(r, y, r, y + rad);
+    ctx.lineTo(r, b - rad);
+    ctx.quadraticCurveTo(r, b, r - rad, b);
+    ctx.lineTo(x + rad, b);
+    ctx.quadraticCurveTo(x, b, x, b - rad);
+    ctx.lineTo(x, y + rad);
+    ctx.quadraticCurveTo(x, y, x + rad, y);
+    ctx.fill();
+}
+
 function drawGame() {
-
-
     ctx.fillStyle = "#bbada0";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canv.width, canv.height);
 
-    for (let i = 0; i < block.coordinates.length; i++) {
-        ctx.fillStyle = "#d6cfc5";
-        ctx.fillRect(block.coordinates[i].x, block.coordinates[i].y, block.size, block.size);
+    for (let i = 0; i < staticBlock.coordinates.length; i++) {
+        ctx.fillStyle = "#d6cdc4";
+        canvasRadius(staticBlock.coordinates[i].x, staticBlock.coordinates[i].y, staticBlock.size, staticBlock.size, 5);
     }
+
+    for (let i = 0; i < blocksWithNumbers.numbers.length; i++) {
+        if (blocksWithNumbers.numbers[i] <= "2048") {
+            ctx.fillStyle = blocksWithNumbers.numberBlocksBackgroundColors[blocksWithNumbers.numbers[i]];
+        }
+        else {
+            ctx.fillStyle = blocksWithNumbers.numberBlocksBackgroundColors.larger;
+        }
+        canvasRadius(blocksWithNumbers.numberBlocksCoordinates[i].x, blocksWithNumbers.numberBlocksCoordinates[i].y, blocksWithNumbers.size, blocksWithNumbers.size, 5);
+    }
+
+
+    for (let i = 0; i < blocksWithNumbers.numbers.length; i++) {
+        if (blocksWithNumbers.numbers[i] === "2" || blocksWithNumbers.numbers[i] === "4") {
+            ctx.fillStyle = "#726763";
+        }
+        else {
+            ctx.fillStyle = "#ffffff";
+        }
+    }
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.font = blocksWithNumbers.numberSize + " Arial";
+    ctx.fillText(blocksWithNumbers.numbers[0], blocksWithNumbers.centeringNumbers[0].x, blocksWithNumbers.centeringNumbers[0].y );
 
 }
 
