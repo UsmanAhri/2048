07004addd03a1e0feaff38ec01b2ea776a3cdab9
UsmanAhri <47859269+UsmanAhri@users.noreply.github.com>
2019-09-10 15:32:37 +0300

    commit for code refactoring

--- js/common.js
+++ js/common.js
@@ -11,13 +11,13 @@ indexItems.forEach((index, i) => {
         indexItems[i].classList.add('selected-size');
         selectedBlockAmount = Number(document.querySelector('.selected-size').value);
 
-        staticBlock.coordinates = [
+        staticBlock.cells = [
             {
                 x: canv.borderSize,
                 y: canv.borderSize
             }
         ];
-        staticBlock.blockSizeCalculation();
+        staticBlock.sizeCalculation();
         staticBlock.staticBlocksCoordinatesRecord();
 
         c.className = 'container';
@@ -50,21 +50,18 @@ function Block() {
 
 Block.prototype = {
     size: Math.round((canv.width - canv.borderSize * (selectedBlockAmount + 1)) / selectedBlockAmount),
-    coordinates: [
+    directionArray: [],
+    cells: [
         {
             x: canv.borderSize,
-            y: canv.borderSize
+            y: canv.borderSize,
+            value: null
         }
     ],
-    centeringNumbers:[],
-    numbers: ["2048"],
-    numberSize: "50px",
-    numberBlocksCoordinates: [
-        {
-            x: canv.borderSize,
-            y: canv.borderSize
-        }
-    ],
+    centeringNumbers: [],
+    numbers: [],
+    numberSize: "30px",
+    numberBlocksCoordinates: [],
     numberBlocksBackgroundColors: {
         2: "#eee4da",
         4: "#ece0c7",
@@ -79,7 +76,7 @@ Block.prototype = {
         2048: "#eec22e",
         larger: "#3d3a33"
     },
-    blockSizeCalculation: function () {
+    sizeCalculation: function () {
         switch (selectedBlockAmount) {
             case 3:
                 this.size = 160;
@@ -113,51 +110,84 @@ Block.prototype = {
                 if (j === 0) {
                     blockX = canv.borderSize;
                 } else {
-                    blockX = this.coordinates[j].x + this.size + canv.borderSize;
+                    blockX = this.cells[j].x + this.size + canv.borderSize;
                 }
 
-                this.coordinates.push({
+                this.cells.push({
                     x: blockX,
                     y: blockY
                 });
             }
-            blockY = this.coordinates[this.coordinates.length - 1].y + this.size + canv.borderSize;
+            blockY = this.cells[this.cells.length - 1].y + this.size + canv.borderSize;
         }
-        this.coordinates.shift();
+        this.cells.shift();
     },
-    centeringNumberCoordinatesRecord: function() {
+    centeringNumberCoordinatesRecord: function () {
+        let centerX, centerY;
+        if (this.centeringNumbers.length < this.numbers.length) {
+            for (let i = 0; i < this.numbers.length; i++) {
+                centerX = this.numberBlocksCoordinates[i].x + Math.round(this.size / 2);
+                centerY = this.numberBlocksCoordinates[i].y + Math.round(this.size / 2);
+            }
+            this.centeringNumbers.push({
+                x: centerX,
+                y: centerY
+            })
+        }
+    },
+    allNumberCoordinatesRecord: function () {
         let centerX, centerY;
 
         for (let i = 0; i < this.numbers.length; i++) {
             centerX = this.numberBlocksCoordinates[i].x + Math.round(this.size / 2);
             centerY = this.numberBlocksCoordinates[i].y + Math.round(this.size / 2);
-             this.centeringNumbers.push({
-                 x: centerX,
-                 y: centerY
-             })
-        }
-    },
-    allNumberCoordinatesRecord: function() {
-        let centerX, centerY;
-
-        for (let i = 0; i < this.numbers.length; i++) {
-            centerX = this.numberBlocksCoordinates[i].x + Math.round(this.size / 2);
-            centerY = this.numberBlocksCoordinates[i].y + Math.round(this.size / 2);
-             this.centeringNumbers.push({
-                 x: centerX,
-                 y: centerY
-             })
+            this.centeringNumbers.push({
+                x: centerX,
+                y: centerY
+            })
         }
     },
     createBlock: function () {
-        console.log(this);
-        if (this.numbers.length < this.coordinates.length) {
-            this.numbers.push("2");
-            this.numberBlocksCoordinates.push(
-                this.coordinates[Math.round(Math.random() * (this.coordinates.length - 1))]
-            )
+        let randomCoordinates = Math.round(Math.random() * (this.cells.length - 1));
+
+        if (this.numbers.length < this.cells.length) {
+            let int = false;
+
+            for (let i = 0; i < this.numberBlocksCoordinates.length; i++) {
+                if (this.cells[randomCoordinates] === this.numberBlocksCoordinates[i]) {
+                    int = true;
+                    break;
+                }
+            }
+            if (int) {
+                this.createBlock();
+            } else {
+                this.numbers.push(2);
+                this.numberBlocksCoordinates.push(
+                    this.cells[randomCoordinates]
+                );
+            }
         }
+    },
+    move: function () {
+        let moveX, moveY;
+
+
+        if (this.directionArray[0] === "right") {
+            for (let i = 0; i < this.cells.length; i++) {
+
+            }
+            this.numberBlocksCoordinates[0].y
+        }
+        this.numberBlocksCoordinates[0].x = this.cells[0].x;
+        this.numberBlocksCoordinates[0].y = this.cells[0].y;
+        this.centeringNumbers.pop();
+        this.centeringNumberCoordinatesRecord();
+
+
+        this.createBlock();
     }
+
 };
 //Not sure if constructor needs to be restored
 Object.defineProperty(Block.prototype, "constructor", {
@@ -170,17 +200,28 @@ let staticBlock = new Block();
 staticBlock.staticBlocksCoordinatesRecord();
 
 let blocksWithNumbers = new Block();
-
-blocksWithNumbers.centeringNumberCoordinatesRecord();
 blocksWithNumbers.createBlock();
 
-function fas() {
-    blocksWithNumbers.createBlock();
+function fas(event) {
+    let cutKeyCode = event.code.substr(5).toLowerCase();
+
+    if (
+        cutKeyCode !== 'up' &&
+        cutKeyCode !== 'down' &&
+        cutKeyCode !== 'right' &&
+        cutKeyCode !== 'left'
+    ) {
+        return false;
+    } else if (cutKeyCode !== blocksWithNumbers.directionArray[0]) {
+        if (blocksWithNumbers.directionArray.length === 3) {
+            blocksWithNumbers.directionArray.pop();
+        }
+        blocksWithNumbers.directionArray.unshift(cutKeyCode);
+        blocksWithNumbers.move();
+    }
 }
 
 document.addEventListener('keydown', fas);
-
-
 
 
 function canvasRadius(x, y, w, h, rad) {
@@ -204,16 +245,15 @@ function drawGame() {
     ctx.fillStyle = "#bbada0";
     ctx.fillRect(0, 0, canv.width, canv.height);
 
-    for (let i = 0; i < staticBlock.coordinates.length; i++) {
+    for (let i = 0; i < staticBlock.cells.length; i++) {
         ctx.fillStyle = "#d6cdc4";
-        canvasRadius(staticBlock.coordinates[i].x, staticBlock.coordinates[i].y, staticBlock.size, staticBlock.size, 5);
+        canvasRadius(staticBlock.cells[i].x, staticBlock.cells[i].y, staticBlock.size, staticBlock.size, 5);
     }
 
     for (let i = 0; i < blocksWithNumbers.numbers.length; i++) {
         if (blocksWithNumbers.numbers[i] <= "2048") {
             ctx.fillStyle = blocksWithNumbers.numberBlocksBackgroundColors[blocksWithNumbers.numbers[i]];
-        }
-        else {
+        } else {
             ctx.fillStyle = blocksWithNumbers.numberBlocksBackgroundColors.larger;
         }
         canvasRadius(blocksWithNumbers.numberBlocksCoordinates[i].x, blocksWithNumbers.numberBlocksCoordinates[i].y, blocksWithNumbers.size, blocksWithNumbers.size, 5);
@@ -221,18 +261,21 @@ function drawGame() {
 
 
     for (let i = 0; i < blocksWithNumbers.numbers.length; i++) {
-        if (blocksWithNumbers.numbers[i] === "2" || blocksWithNumbers.numbers[i] === "4") {
+        if (blocksWithNumbers.numbers[i] === 2 || blocksWithNumbers.numbers[i] === 4) {
             ctx.fillStyle = "#726763";
-        }
-        else {
+        } else {
             ctx.fillStyle = "#ffffff";
         }
     }
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.font = blocksWithNumbers.numberSize + " Arial";
-    ctx.fillText(blocksWithNumbers.numbers[0], blocksWithNumbers.centeringNumbers[0].x, blocksWithNumbers.centeringNumbers[0].y );
-
+    if (blocksWithNumbers.numbers[0]) {
+        blocksWithNumbers.centeringNumberCoordinatesRecord();
+        for (let i = 0; i < blocksWithNumbers.centeringNumbers.length; i++) {
+            ctx.fillText(blocksWithNumbers.numbers[i], blocksWithNumbers.centeringNumbers[i].x, blocksWithNumbers.centeringNumbers[i].y);
+        }
+    }
 }
 
 setInterval(drawGame, 50);
